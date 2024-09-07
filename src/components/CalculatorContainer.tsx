5caa8fb707c87874a78342994883ac26a93e8dbb
z0opreme <remy.engassac@icloud.com>
2024-09-07 23:44:13 +0200

    x

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -1,11 +1,15 @@
 import Screen from './Screen'
 import Keypad from './Keypad'
+import TextAuthor from './TextAuthor'
 
 export default function Calculator() {
 	return (
-		<div className="bg-black w-[95dvw] md:w-[90dvw] max-w-[400px] h-[70dvh] md:h-[80dvh] max-h-[700px] rounded-2xl shadow-lg shadow-stone-800 mx-auto my-4">
-			<Screen />
-			<Keypad />
+		<div className="bg-black w-[95dvw] md:w-[90dvw] max-w-[400px] h-[80dvh] md:h-[85dvh] max-h-[700px] rounded-2xl shadow-lg shadow-stone-800 mx-auto my-4 flex flex-col justify-between">
+			<div>
+				<Screen />
+				<Keypad />
+			</div>
+			<TextAuthor />
 		</div>
 	)
 }
---
+++ src/components/TextAuthor.tsx
@@ -0,0 +1,7 @@
+export default function TextAuthor() {
+	return (
+		<div className="text-white text-sm text-center mb-2">
+			<p>Calculator made with ❤️ by <a href="https://github.com/https-remy" target="_blank" className="text-orange-500 underline">Rémy</a> !</p>
+		</div>
+	);
+}
