96f406ffefb2d27b81aaa4fd3bf65c3408f86aec
z0opreme <remy.engassac@icloud.com>
2024-09-07 18:37:08 +0200

    set base on vite config 3

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -3,7 +3,7 @@ import Keypad from './Keypad'
 
 export default function Calculator() {
 	return (
-		<div className="bg-black max-w-md min-w-[400px] h-[65dvh] w-[45dvw] rounded-2xl shadow-lg shadow-zinc-800">
+		<div className="bg-black max-w-md min-w-[400px] h-[65dvh] w-[45dvw] rounded-2xl shadow-lg shadow-stone-800">
 			<Screen />
 			<Keypad />
 		</div>
