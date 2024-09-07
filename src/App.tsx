e39f87f0ea91d0085b21d878f836f6e7400460ba
z0opreme <remy.engassac@icloud.com>
2024-09-07 23:51:52 +0200

    x

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CalculatorContainer from './components/CalculatorContainer'
 export default function App() {
 	return (
 		<div className="h-[100dvh] w-[100dvw] flex flex-col items-center justify-center">
-			<h1 className="text-white text-4xl font-bold mb-4 md:mb-8 underline">Calculator App 🧮</h1>
+			<h1 className="text-white text-3xl md:text-4xl font-bold mb-4 md:mb-8 underline">Calculator App 🧮</h1>
 			<CalculatorContainer />
 		</div>
 	)
