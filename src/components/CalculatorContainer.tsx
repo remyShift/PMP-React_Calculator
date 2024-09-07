8af10d71117a309b23c93249b246da96260ded53
z0opreme <remy.engassac@icloud.com>
2024-09-07 23:48:28 +0200

    x

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -4,7 +4,7 @@ import TextAuthor from './TextAuthor'
 
 export default function Calculator() {
 	return (
-		<div className="bg-black w-[95dvw] md:w-[90dvw] max-w-[400px] h-[80dvh] md:h-[85dvh] max-h-[700px] rounded-2xl shadow-lg shadow-stone-800 mx-auto my-4 flex flex-col justify-between">
+		<div className="bg-black w-[95dvw] md:w-[90dvw] max-w-[400px] h-[80dvh] min-h-[650px] md:h-[85dvh] max-h-[700px] rounded-2xl shadow-lg shadow-stone-800 mx-auto flex flex-col justify-between">
 			<div>
 				<Screen />
 				<Keypad />
