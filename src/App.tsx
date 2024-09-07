c1eb2b154d6cf36390b6c2f1b812e707ab51716d
z0opreme <remy.engassac@icloud.com>
2024-09-07 19:02:57 +0200

    x

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CalculatorContainer from './components/CalculatorContainer'
 export default function App() {
 	return (
 		<div className="h-screen w-screen flex flex-col items-center justify-center">
-			<h1 className="text-white text-4xl font-bold mb-4 underline-offset-0">Calculator App 🧮</h1>
+			<h1 className="text-white text-4xl font-bold mb-8 underline">Calculator App 🧮</h1>
 			<CalculatorContainer />
 		</div>
 	)
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -21,26 +21,26 @@ export default function NumberButton({ children }: { children: React.ReactNode }
 
 	if (children === 'AC' || children === '+/-' || children === '%') {
 		return (
-			<button onClick={handleClick} className="bg-stone-500 rounded-[50%] w-24 h-24 flex items-center justify-center text-white text-4xl hover:bg-stone-400 transition-colors duration-200">
+			<button onClick={handleClick} className="bg-stone-500 rounded-[50%] w-20 h-20 flex items-center justify-center text-white text-4xl hover:bg-stone-400 transition-colors duration-200">
 				{children}
 			</button>
 		)
 	}
 	if (typeof children === 'string' && children !== ',') {
 		return (
-			<button onClick={handleClick} className="bg-orange-500 rounded-[50%] w-24 h-24 flex items-center justify-center text-white text-4xl hover:bg-orange-400 transition-colors duration-200">
+			<button onClick={handleClick} className="bg-orange-500 rounded-[50%] w-20 h-20 flex items-center justify-center text-white text-4xl hover:bg-orange-400 transition-colors duration-200">
 				{children}
 			</button>
 		)
 	} else if (children === 0) {
 		return (
-			<button onClick={handleClick} className="bg-stone-900 rounded-full w-[200px] h-24 flex items-center justify-start pl-7 text-white text-3xl hover:bg-stone-800 transition-colors duration-200">
+			<button onClick={handleClick} className="bg-stone-900 rounded-full w-[175px] h-20 flex items-center justify-start pl-7 text-white text-3xl hover:bg-stone-800 transition-colors duration-200">
 				{children}
 			</button>
 		)
 	}
 	return (
-		<button onClick={handleClick} className="bg-stone-900 rounded-[50%] w-24 h-24 flex items-center justify-center text-white text-3xl hover:bg-stone-800 transition-colors duration-200">
+		<button onClick={handleClick} className="bg-stone-900 rounded-[50%] w-20 h-20 flex items-center justify-center text-white text-3xl hover:bg-stone-800 transition-colors duration-200">
 			{children}
 		</button>
 	);
